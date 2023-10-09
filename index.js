a2f7bd52af1c16f2ae50b1ff4eb77478b41a2451
Hases00 <carlosegomez001@hotmail.com>
2023-10-09 09:58:38 -0400

    update

---
+++ index.js
@@ -0,0 +1,40 @@
+// Prompt the user for a list of integers separated by commas.
+const userInputString = prompt(
+  "Please enter your Froyo flavors, we have Vanilla, Strawberry and coffee for now. Use the example and replace with your selection:",
+  (placeholder = " Vanilla - Vanilla - Vanilla - Strawberry - Coffee - Coffee")
+);
+
+// Split the string of numbers into an array of strings.
+const flavors = userInputString.split("-");
+
+let flavorCount = {};
+for (let i = 0; i < flavors.length; i++) {
+  let flavor = flavors[i].trim();
+  if (flavorCount[flavor]) {
+    flavorCount[flavor]++;
+  } else {
+    flavorCount[flavor] = 1;
+  }
+}
+
+console.log(`You have given ${getLength(flavors)} orders.`);
+console.log(`This is your final order is = ${flavors}`);
+console.table(flavors);
+// === EDIT THE CODE BELOW ===
+// Complete the functions below to make the program work!
+
+/**
+ * @param {string[]} flavors an array of integers
+ * @returns {string} the length of the array
+ */
+function getLength(flavors) {
+  return flavors.length;
+}
+
+// /**
+//  * @param {string[]} flavors an array of integers
+//  * @returns {string} the sum of the flavors
+//  */
+// function getSum(flavors) {
+//   return flavors.reduce((a, b) => a + b);
+// }
